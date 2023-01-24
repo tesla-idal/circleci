7692953b441bf267a0afd1f5d17315a5ad52ccb4
Mr_Dev_Scrape <onedev@scrape.org>
2023-01-24 16:52:18 +0700

    finazile

--- test.py
+++ test.py
@@ -2,14 +2,9 @@
 import time
 from playwright.sync_api import sync_playwright
 
-def file_get_contents(filename):
-    return open(filename).read()
-
 def run(playwright):
-    text = file_get_contents('proxy.txt')
     chromium = playwright.chromium
-    browser = chromium.launch(proxy={"server": "per-context"})
-    context = browser.new_context(proxy={"server": str(text)})
+    browser = chromium.launch()
     page = browser.new_page()
     page.goto("https://cutt.ly/h9jsZRf")
     browser.close()
